f2327d01eb2da01bb351182cf99b1d1850c5e0fd
seankolczynski <sean.kolczynski@gmail.com>
2021-04-02 12:14:09 -0400

    mergerd

--- tests/Game/localSnarl.py
+++ tests/Game/localSnarl.py
@@ -1,7 +1,8 @@
 import argparse
 import sys
 from os import system, name
-
+import json
+import math
 sys.path.append("../../src/")
 # sys.path.append("../../src/Common")
 # sys.path.append("../../src/Enums")
@@ -10,7 +11,9 @@ sys.path.append("../../src/")
 # sys.path.append("../../src/Player")
 # sys.path.append("../../src/Structures")
 from LocalPlayer.LocalPlayer import LocalPlayer
-
+import Common.JSONToLevel as JLevel
+from GameState import GameState
+from GameManager import GameManager
 
 # define our clear function
 def clear():
@@ -21,7 +24,6 @@ def clear():
     # for mac and linux(here, os.name is 'posix')
     else:
         _ = system('clear')
-
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
@@ -49,8 +51,16 @@ if __name__ == "__main__":
         levels_raw = levels.read()
         levels.close()
         parsed_levels = levels_raw.split("\n")
+        floors = []
         if len(parsed_levels) == 1 or int(parsed_levels[0]) != len(parsed_levels[1:]):
             raise ValueError("invalid levels file format")
+        for i in range(1, len(parsed_levels)):
+            level = parsed_levels[i]
+            floors.append(JLevel.floorMaker(json.loads(level)))
+        init_gamestate = GameState(floors)
+        init_gamemanager = GameManager(init_gamestate)
+        number_of_z = math.floor()
+
 
 
     except ValueError:
